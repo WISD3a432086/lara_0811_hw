0230bb40e88e9751ea2d8df242cd72ad7aa9bc2a
{3a432086} <{tim5556618@gmail.com}>
2017-10-26 22:48:50 +0800

    修改controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,5 +6,20 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    //
+  public function home()
+  {
+    return view('home');
+  }
+
+    public function about()
+    {
+        return view('about');
+    }
+
+    public function news()
+    {
+        return view('news');
+    }
+
+
 }
